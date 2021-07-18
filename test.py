3cf81014e0996cfa9c5dbf787750bc2dfb156dfd
Cemal Tiryaki <53711541+sadeceben@users.noreply.github.com>
2021-07-18 22:32:48 +0300

    added: test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import httpx
+
+class TEST:
+
+    def __init__(self):
+        self.url = 'https://test.com'
+
+    def get_content(self):
+        return httpx.get(self.url).text
+
+if __name__ == '__main__':
+    run = TEST()
+    print(run.get_content())
